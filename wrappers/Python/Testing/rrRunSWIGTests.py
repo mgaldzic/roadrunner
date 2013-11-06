2a938b767567ce53c0fa297ac9f7d707abb2868b
Michal Galdzicki <mgaldzic@gmail.com>
2013-11-06 00:38:14 -0800

    first 4 python swig api tests: test_ReactionIds, test_SetSteadyStateSelectionList, test_SetSteadyStateSelectionList, test_load

---
+++ wrappers/Python/Testing/rrRunSWIGTests.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+##@Module rrRunSWIGTests
+"""
+Created on Tue Nov 05 20:17:20 2013
+
+@author: mgaldzic
+"""
+#import sys
+import unittest
+import roadrunner # the swig roadrunner
+#import random
+#import string
+#from numpy import *
+
+
+RPADDING = 45
+def passMsg (errorFlag):
+    if errorFlag:
+        return "*****FAIL*****"
+    else:
+        return "PASS"
+
+class TestRoadRunnerFunctions(unittest.TestCase):
+    r = roadrunner.RoadRunner()
+    r.load('simple_test_model.xml')    
+    
+    def setUp(self):
+        pass
+        #self.r = roadrunner.RoadRunner()
+        #self.r.load('simple_test_model.xml')
+        #r.setConservationLaw(false) #TODO if needed
+        # initialization sets this to false - no equivalent in swigapi?
+        
+    def test_SetSteadyStateSelectionList(self):
+        print "test_SetSteadyStateSelectionList"
+        line ="S1 S2 S3"
+	actual = self.r.steadyStateSelections = line.split()
+	self.assertTrue(actual)
+
+    def test_ReactionIds(self):
+        print "test_ReactionIds"
+        line = "J1 J2 J3 J4"
+        expected = line.split()
+        actual = self.r.model.getReactionIds()
+	self.assertItemsEqual(expected, actual)
+    
+    def test_load(self):
+        print "test_load"
+        #print self.r.getInfo()
+        self.assertTrue(self.r.load('simple_test_model.xml'))
+    
+
+    def test_isModelLoaded(self):
+        print "test_isModelLoaded"
+        print self.r.getuCC ('J1', 'k1') #Get unscaled control coefficient with
+                                         #respect to a global parameter
+        #print "getuCC J1"
+        #print "k_1 " + str(self.r.getuCC ('J1', 'k_1'))
+        #print "k2  " + str(self.r.getuCC ('J1', 'k2'))
+        #print "k_2 " + str(self.r.getuCC ('J1', 'k_2'))
+        #print "k3  " + str(self.r.getuCC ('J1', 'k3'))
+        #print "k_3 " + str(self.r.getuCC ('J1', 'k_3'))
+        #print "k4  " + str(self.r.getuCC ('J1', 'k4'))
+        #print "k_4 " + str(self.r.getuCC ('J1', 'k_4'))
+        self.assertTrue(self.r.isModelLoaded())
+
+        
+    #@unittest.skip("setConservationLaw cant find equivalent")
+    #def test_setConservationLaw():
+    #    pass
+
+    #    line = readLine ()
+    #    if line == 'True':
+    #        rrPython.setComputeAndAssignConservationLaws(1)
+    #    else:
+    #        rrPython.setComputeAndAssignConservationLaws(0)
+
+if __name__ == '__main__':
+    unittest.main()
